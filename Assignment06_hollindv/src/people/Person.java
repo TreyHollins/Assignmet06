6d38bfd11176a5cfa3d83f48f17cb4ec4b9963ae
Trey Hollins <trey.hollins@gmail.com>
2020-02-24 12:14:13 -0500

    First commit. Added Person and Main classes

---
+++ Assignment06_hollindv/src/people/Person.java
@@ -0,0 +1,14 @@
+/*
+ * Name: David Hollins
+ * Email: hollindv@mail.uc.edu
+ * Course: IT 2045C
+ * Due Date: 2/27/2020
+ * Assignment: 06
+ * Description:
+ * Citations:
+ */
+package people;
+
+public abstract class Person {
+
+}
