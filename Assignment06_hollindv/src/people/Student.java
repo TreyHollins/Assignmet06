0122d006a1b19831504de9c8c4ca4a425d3d6749
Trey Hollins <trey.hollins@gmail.com>
2020-02-26 11:18:19 -0500

    Added comments

--- Assignment06_hollindv/src/people/Student.java
+++ Assignment06_hollindv/src/people/Student.java
@@ -1,3 +1,12 @@
+/*
+ * Name: David Hollins
+ * Email: hollindv@mail.uc.edu
+ * Course: IT 2045C
+ * Due Date: 2/27/2020
+ * Assignment: 06
+ * Description:
+ * Citations:
+ */
 package people;
 
 public class Student extends Person{
